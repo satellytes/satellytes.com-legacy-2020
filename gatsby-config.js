87e3371beac3cb1a559eeea9bc1bd59f025d2346
Georgios Kaleadis <>
2018-10-13 20:03:07 +0200

    chore: update manifest with new colors and labels

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,11 +49,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Satellytes',
+        short_name: 'Satellytes',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#202840',
+        theme_color: '#202840',
         display: 'minimal-ui',
         icon: 'src/images/satellytes-icon.png', // This path is relative to the root of the site.
       },
