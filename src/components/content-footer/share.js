bcdabce9dfe8ec0cf5abedfcc34247662e889842
Georgios Kaleadis <>
2018-10-13 08:47:06 +0200

    feat: add linkedin share button

--- src/components/content-footer/share.js
+++ src/components/content-footer/share.js
@@ -1,8 +1,16 @@
-import {EmailShareButton, WhatsappShareButton, TwitterShareButton, FacebookShareButton} from 'react-share';
+import {
+  EmailShareButton,
+  WhatsappShareButton,
+  TwitterShareButton,
+  FacebookShareButton,
+  LinkedinShareButton
+} from 'react-share';
+
 import React from "react";
 import IconTwitter2 from '../../assets/svg/icon-twitter2.svg';
 import IconMail from '../../assets/svg/icon-mail.svg';
 import IconFacebook from '../../assets/svg/icon-facebook.svg';
+import IconLinkedin from '../../assets/svg/icon-linkedin-2.svg';
 import IconWhatsapp from '../../assets/svg/icon-whatsapp.svg';
 import styled from 'styled-components';
 import { HeadlineParagraph } from "../typography/headline";
@@ -44,6 +52,12 @@ export const Share = ({location, data}) => {
           <FacebookShareButton url={url}>
             <IconFacebook/>
           </FacebookShareButton>
+        </ShareButton>
+
+        <ShareButton>
+          <LinkedinShareButton url={url}>
+            <IconLinkedin/>
+          </LinkedinShareButton>
         </ShareButton>
 
         <ShareButton>
